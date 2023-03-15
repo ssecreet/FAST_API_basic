3626a043d033103ff09ad8c08faf83b1ab9290ec
ssecreet <120476413+ssecreet@users.noreply.github.com>
2023-03-15 18:06:22 +0300

    Add files via upload

---
+++ tests/users/test_update_user.py
@@ -0,0 +1,21 @@
+import random
+import string
+
+from tests.configuration import ACCESSED_CATALOG_ENUM
+
+
+def test_positive(user_fixture):
+    # precondition - предусловие. Создание данных
+    user_id = 1
+    username = "test_" + "".join(random.sample(string.ascii_letters, 5))
+    age = random.randint(0, 120)
+    address = "test_address"
+    accessed_catalog = {
+        "name": "test_" + "".join(random.sample(string.ascii_letters, 5)),
+        "catalog": random.choice(ACCESSED_CATALOG_ENUM)
+    }
+
+    # request execution
+    response = user_fixture.api_client.user.update_user(user_id, username, age, address, accessed_catalog)
+
+    assert response.status_code == 200, "Статус код не соответствует ожидаемому"
